a78fae565ca956a66cd591d8b671d5a16ecf5774
Baysah Korti <baysahkorti@gmail.com>
2023-07-19 15:14:08 -0400

    Update Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="container py-5">
       <h1 className="text-5xl font-bold tracking-tight text-slate-600 sm:text-[4rem] text-center">JREF URL Shortner</h1>
-      <h2 className='text-2xl text-center text-slate-500'>JREF URL Shortner coming soon</h2>
+      <h2 className='text-2xl text-center text-slate-400'>JREF URL Shortner coming soon</h2>
     </main>
   )
 }
