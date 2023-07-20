21777cf6e8ab7d6024e08a0ac4f130d4a45b24c3
Baysah Korti <baysahkorti@gmail.com>
2023-07-20 06:49:16 -0400

    Removed hard coded db string

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,13 @@
 import { neon } from '@neondatabase/serverless';
 
-//const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
-const sql = neon(
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+/* const sql = neon(
   'postgres://baysahkorti:4OyVwNC7iFtm@ep-ancient-recipe-701031.us-east-1.aws.neon.tech/neondb'
-);
+); */
 export async function helloWorld() {
   const start = new Date();
   const [dbResponse] = await sql`SELECT NOW();`;
   const dbNow = dbResponse && dbResponse.now ? dbResponse.now : '';
   const end = new Date();
-  return {dbNow: dbNow, latency: end - start};
+  return { dbNow: dbNow, latency: end - start };
 }
